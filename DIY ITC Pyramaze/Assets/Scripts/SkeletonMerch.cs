0e3516bf32a05ed9e5909c0d769307d7ae13f8ef
Johnny Boi <jah22d@acu.edu>
2023-11-30 15:14:59 -0600

    Shop weapons are now shown and are random
    the shop weapons are 3 different images: whip, sword, and gun. Each have a 1/3 chance of being sold in the shop.

--- DIY ITC Pyramaze/Assets/Scripts/SkeletonMerch.cs
+++ DIY ITC Pyramaze/Assets/Scripts/SkeletonMerch.cs
@@ -1,20 +1,34 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor.Recorder;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class SkeletonMerch : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        ranATK = Random.Range(1, 4);
     }
 
     public GameObject gameObject;
-
     public GameObject gameObject2;
+
+    public UnityEngine.UI.Image gun;
+    public UnityEngine.UI.Image whip;
+    public UnityEngine.UI.Image sword;
+
+    public bool setWeapon = true;
+    
+    public int ranATK;
+
+    
 
     // Update is called once per frame
     void Update()
@@ -35,6 +49,29 @@ public class SkeletonMerch : MonoBehaviour
             collision.gameObject.transform.position = new Vector3(10000, 10000, 0);
 
             collision.gameObject.GetComponent<TopDownPlayer>().playerMove = false;
+
+            if (setWeapon)
+            {
+                setWeapon = false;
+                if (ranATK == 1)
+                {
+                    gun.enabled = true;
+                    whip.enabled = false;
+                    sword.enabled = false;
+                }
+                if (ranATK == 2)
+                {
+                    gun.enabled = false;
+                    whip.enabled = true;
+                    sword.enabled = false;
+                }
+                if (ranATK == 3)
+                {
+                    gun.enabled = false;
+                    whip.enabled = false;
+                    sword.enabled = true;
+                }
+            }
         }
     }
 }
