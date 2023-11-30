447143da8b73fe1d6be3abf301271c9c01e93f87
Johnny Boi <jah22d@acu.edu>
2023-11-30 10:35:44 -0600

    Shop and Player Update
    Player can now touch Skeleton Merchant to enter the shop (haven't programmed an exit yet). Player now also has a boolean "playerMove", which starts as true, but is set to false upon entering the shop

---
+++ DIY ITC Pyramaze/Assets/Scripts/SkeletonMerch.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkeletonMerch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject gameObject;
+
+    public GameObject gameObject2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(true);
+            
+            //gameObject2.SetActive(false);
+
+            collision.gameObject.transform.Translate(new Vector3(50, 0, 0));
+
+            collision.gameObject.GetComponent<TopDownPlayer>().playerMove = false;
+        }
+    }
+}
--- DIY ITC Pyramaze/Assets/Scripts/TopDownPlayer.cs
+++ DIY ITC Pyramaze/Assets/Scripts/TopDownPlayer.cs
@@ -1,20 +1,28 @@
 
+using System;
 using UnityEngine;
 
 public class TopDownPlayer : MonoBehaviour
 {
     public float moveSpeed = 10.0f;
 
+    public Boolean playerMove = true;
+
     // Update is called once per frame
     void Update()
     {
-        // What Moves Us
-        float horizontalInput = Input.GetAxis("Horizontal");
 
-        //Get the value of the Horizontal input axis.
-        float verticalInput = Input.GetAxis("Vertical");
+        if (playerMove)
+        {
+            // What Moves Us
+            float horizontalInput = Input.GetAxis("Horizontal");
 
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
-        //Move the object to XYZ coordinates defined as horizontalInput, verticalInput, and 0 respectively.
+            //Get the value of the Horizontal input axis.
+            float verticalInput = Input.GetAxis("Vertical");
+
+            transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
+            //Move the object to XYZ coordinates defined as horizontalInput, verticalInput, and 0 respectively.
+        }
+        
     }
 }
