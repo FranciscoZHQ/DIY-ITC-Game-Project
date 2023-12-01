bd3de29bc09572fa7950b95f46434ce096a53012
Johnny Boi <jah22d@acu.edu>
2023-12-01 00:15:56 -0600

    Minor Changes
    Fixed shop, shop is complete until weapons are finished

--- DIY ITC Pyramaze/Assets/Scripts/GameManager.cs
+++ DIY ITC Pyramaze/Assets/Scripts/GameManager.cs
@@ -7,9 +7,11 @@ public class GameManager : MonoBehaviour
 {
     public int numberOfCoins = 0;
     public int numberOfRubys = 0;
+    public int numberOfPotions = 0;
 
     public TextMeshProUGUI coinsText;
     public TextMeshProUGUI rubyText;
+    public TextMeshProUGUI potionText;
 
     public Vector3 spawnPoint;
 
@@ -24,5 +26,36 @@ public class GameManager : MonoBehaviour
     {
         coinsText.text = "Coins: " + numberOfCoins;
         rubyText.text = "Rubys: " + numberOfRubys;
+        potionText.text = "Potions: " + numberOfPotions;
+    }
+
+    public void buyPotion()
+    {
+        if (numberOfCoins >= 15)
+        {
+            numberOfCoins -= 15;
+            numberOfPotions++;
+        }
+    }
+
+    public void buyWeapon()
+    {
+        if (numberOfRubys >= 3)
+        {
+            numberOfRubys -= 3;
+
+            //if (GetComponent<SkeletonMerch>().ranATK == 1)
+            //{
+            //// Insert code to change weapon to gun
+            //}
+            //if (GetComponent<SkeletonMerch>().ranATK == 2)
+            //{
+            //// Insert code to change weapon to whip
+            //}
+            //if (GetComponent<SkeletonMerch>().ranATK == 3)
+            //{
+            //// Insert code to change weapon to sword
+            //}
+        }
     }
 }
