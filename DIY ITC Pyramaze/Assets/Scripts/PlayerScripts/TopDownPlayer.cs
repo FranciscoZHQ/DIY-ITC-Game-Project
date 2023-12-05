deca4e67007b0b7ba3f7c887b8e615ca889981a9
Logan-Rodriguez <ljr23a@acu.edu>
2023-12-05 15:06:46 -0600

    Logans big merge
    Checked everything

--- DIY ITC Pyramaze/Assets/Scripts/PlayerScripts/TopDownPlayer.cs
+++ DIY ITC Pyramaze/Assets/Scripts/PlayerScripts/TopDownPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TopDownPlayer : MonoBehaviour
 {
-    public float moveSpeed = 10.0f;
+    public float moveSpeed = 8.0f;
 
     public Boolean playerMove = true;
 
