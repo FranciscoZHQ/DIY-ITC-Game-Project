5b4b46bc692576805b9d20ee01cada4dbc3dedc8
Johnny Boi <jah22d@acu.edu>
2023-12-05 16:21:51 -0600

    BossATK

--- DIY ITC Pyramaze/Assets/Scripts/Boss Scripts/BossBehavior.cs
+++ DIY ITC Pyramaze/Assets/Scripts/Boss Scripts/BossBehavior.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BossBehavior : MonoBehaviour
@@ -7,15 +8,13 @@ public class BossBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        makeSlimeDup();
-        makeSlimeDup();
-        makeSlimeDup();
+        StartCoroutine(wait());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //makeSlimeDup();
+        
     }
 
     public GameObject gameObject;
@@ -23,5 +22,24 @@ public class BossBehavior : MonoBehaviour
     {
         GameObject mini = GameObject.Instantiate(gameObject);
         mini.transform.position = transform.position;
+    }
+
+    IEnumerator waitMake()
+    {
+        makeSlimeDup();
+        yield return new WaitForSeconds(0.1f);
+        makeSlimeDup();
+        yield return new WaitForSeconds(0.1f);
+        makeSlimeDup();
+        yield return new WaitForSeconds(0.1f);
+        makeSlimeDup();
+        yield return new WaitForSeconds(0.1f);
+        makeSlimeDup();
+        StartCoroutine(wait());
+    }
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(5.0f);
+        StartCoroutine(waitMake());
     }
 }
