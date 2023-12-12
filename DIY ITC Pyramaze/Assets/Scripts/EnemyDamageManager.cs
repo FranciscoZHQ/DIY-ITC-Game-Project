3890753b6b640ce94311ed325a37b5e54397dfda
Logan-Rodriguez <ljr23a@acu.edu>
2023-12-12 11:02:44 -0600

    Another merge for this branch

---
+++ DIY ITC Pyramaze/Assets/Scripts/EnemyDamageManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamageManager : MonoBehaviour
+{
+    PlayerHealth health;
+    HealthBar healthBar;
+    private bool isColliding = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+       healthBar = GameObject.Find("Health Bar").GetComponent<HealthBar>();
+       health = GameObject.Find("Protag-Kun_0").GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (isColliding)
+        {
+            return;
+        }
+
+        if (other.gameObject.tag == "Player")
+
+        {
+            isColliding = true;
+            health.playerCurrentHealth = health.playerCurrentHealth - 10;
+            healthBar.slider.value = health.playerCurrentHealth;
+            StartCoroutine(Reset()); // I-frames.
+        }
+    }
+
+    // I-frames.
+
+    IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(1.75f);
+        isColliding = false;
+    }
+}
